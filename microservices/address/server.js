6a5110a6eb90e05d0b7dbfa61ee90928305ea9fa
Jordan Lee <jordanlee133@gmail.com>
2017-09-25 14:53:26 -0400

    fixed server file

--- microservices/address/server.js
+++ microservices/address/server.js
@@ -1,10 +1,10 @@
-const express = require('express)
+const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, function() {
+app.listen(3000, function() {
   console.log('Address microservice listening on port 3000!')
 })
