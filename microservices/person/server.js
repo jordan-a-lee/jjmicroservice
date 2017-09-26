ccd1547d822329dc10c7b72488a475e883f04c11
Jake Kwon <jhnana0629@gmail.com>
2017-09-25 20:11:58 -0400

    get person on their id

--- microservices/person/server.js
+++ microservices/person/server.js
@@ -17,37 +17,68 @@ AWS.config.loadFromPath('../config.json');
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {
- ExpressionAttributeValues: {
-  ":v1": {
-    S: "1"
-   }
- },
- KeyConditionExpression: "ID = :v1",
- TableName: "Person"
-};
-dynamodb.query(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(JSON.stringify(data));           // successful response
-  /*
-  data = {
-   ConsumedCapacity: {
+
+// dynamodb.query(params, function(err, data) {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else     console.log(JSON.stringify(data));           // successful response
+//   /*
+//   data = {
+//    ConsumedCapacity: {
+//    },
+//    Count: 2,
+//    Items: [
+//       {
+//      "SongTitle": {
+//        S: "Call Me Today"
+//       }
+//     }
+//    ],
+//    ScannedCount: 2
+//   }
+//   */
+// });
+
+app.get('/person/:personid', function(req, res) {
+  console.log(req.params.personid);
+  var personid = req.params.personid;
+  var params = {
+   ExpressionAttributeValues: {
+    ":v1": {
+      S: personid
+     }
    },
-   Count: 2,
-   Items: [
-      {
-     "SongTitle": {
-       S: "Call Me Today"
-      }
-    }
-   ],
-   ScannedCount: 2
-  }
-  */
+   KeyConditionExpression: "ID = :v1",
+   TableName: "Person"
+  };
+
+  dynamodb.query(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    // else     console.log(JSON.stringify(data));           // successful response
+    else   res.json(data);
+  });
 });
 
+app.get('/person/lastname/:lastname', function(req, res) {
+  console.log(req.params.lastname);
+  var lastname = req.params.lastname;
+  var params = {
+   ExpressionAttributeValues: {
+    ":v1": {
+      S: lastname
+     }
+   },
+   KeyConditionExpression: "LastName = :v1",
+   TableName: "Person"
+  };
 
-app.use('/api', router);
+  dynamodb.query(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    // else     console.log(JSON.stringify(data));           // successful response
+    else   res.json(data);
+  });
+});
+
+// app.use('/:personid', router);
 
 app.listen(port);
 console.log('Port listening on ' + port);
